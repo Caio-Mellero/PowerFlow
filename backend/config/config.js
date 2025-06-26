4a3fe224f83020eea05682b377272d7101d06840
Caio de Sousa Mellero <caiodesousamellero@gmail.com>
2025-06-25 21:50:40 -0300

    Configuração do Sequelize e modelos de User/Task

---
+++ backend/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config(); // IMPORTANTE: Carrega as variáveis do .env
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "powerflow_test_db", // Geralmente usa-se um DB diferente para testes
+    host: process.env.DB_HOST,
+    dialect: "postgres"
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: "postgres"
+  }
+};
